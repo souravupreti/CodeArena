e145fe76731fcc6269f3f0ca10395dbccfd7461e
souravupreti <souravupreti1@gmail.com>
2026-07-24 16:59:12 +0530

    fix

--- backend/src/controllers/userAuthent.js
+++ backend/src/controllers/userAuthent.js
@@ -73,7 +73,12 @@ const login = async (req,res)=>{
         }
 
         const token =  jwt.sign({_id:user._id , emailId:emailId, role:user.role},process.env.JWT_KEY,{expiresIn: 60*60});
-        res.cookie('token',token,{maxAge: 60*60*1000});
+        res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 60 * 60 * 1000,
+});
         res.status(200).json({
             user:reply,
             message:"Logged in Successfully"
